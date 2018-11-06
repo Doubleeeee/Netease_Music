3a7a0d62c6e5c6c9f777e7de16406af44b24a873
qzy <qziyu_alris@163.com>
2018-11-06 15:30:14 +0800

    add friend

---
+++ pages/friend/friend.js
@@ -0,0 +1,18 @@
+Component({
+  properties: {
+    // 定义innerText属性，可在组件使用时指定
+    
+  },
+  // 私有数据，组件的初始数据
+  data: {
+    // 一些组件内部数据
+    
+  },
+  methods: {
+    // 自定义方法
+
+  },
+  onLoad:function(){
+    
+  }
+})
